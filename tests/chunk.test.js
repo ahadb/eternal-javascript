b775f9f45e36ea4a85e95bc4c34aecae2b415a74
ahadb <ahadbokhari@gmail.com>
2022-10-09 15:07:58 -0400

    chunkArray

---
+++ tests/chunk.test.js
@@ -0,0 +1,23 @@
+const chunkArray = require("../src/chunkArray");
+
+describe("chunk", () => {
+  test("should create 3 chunks based on size of 2", () => {
+    const chunkedArray = chunkArray([1, 2, 3, 4, 5], 2);
+
+    expect(chunkedArray).toEqual([[1, 2], [3, 4], [5]]);
+  });
+
+  test("should create 5 single array chunks based on size of 1", () => {
+    const chunkedArray = chunkArray([1, 2, 3, 4, 5], 1);
+
+    expect(chunkedArray).toEqual([[1], [2], [3], [4], [5]]);
+  });
+
+  test("should handle a larger array and create 1000 chunks based on size of 1", () => {
+    const arrKeys = Array.from(Array(1000).keys());
+    const chunkedArray = chunkArray(arrKeys, 1);
+
+    expect(chunkedArray[chunkedArray.length - 1]).toEqual([999]);
+    //console.log(chunkedArray[0])
+  });
+});
