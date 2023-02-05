527557e03c4a3f481fcd6405164f5b6369732ec8
ahadb <ahadbokhari@gmail.com>
2023-02-05 16:08:16 -0500

    magic values continued

--- src/day-to-day/codeSmellsMagicValues.js
+++ src/day-to-day/codeSmellsMagicValues.js
@@ -3,14 +3,14 @@
  * Magic values refer to variables that we assign values
  * to. They have some kind of meaning but can cause
  * confusion if not readable by others. We can't expect
- * people reading our code to understand it if you don't
+ * people reading our code to understand it if we don't
  * write with the goal of readbility in mind
  *
  */
 
 // #1
-// if x is changed to another string we have a problem
-// this won't scale
+// If x is changed to another string we have a problem
+// and this won't scale
 let x = "initialize";
 let y = "";
 
@@ -18,7 +18,16 @@ for (let i = 0; i < 10; i++) {
   b += y[i].toUpperCase();
 }
 
-// this is a better approach
+// This is a better approach
 for (let i = 0; i < x.length; i++) {
   b += y[i].toUpperCase();
 }
+
+// #2
+// Intent and readability with primitives or other data types
+// We're not helping anyone without some kind of explanation
+let totalCost = orders.getAll() * 1.44;
+
+// This is a better approach
+const GLOBAL_TAX_RATE = 1.44;
+let totalCost = orders.getAll() * GLOBAL_TAX_RATE;
