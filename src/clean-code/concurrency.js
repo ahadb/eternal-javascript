c23b6bd222bc8846bad1ecda2e0e32e760f742c0
Ahad Bokhari <ahadbokhari@gmail.com>
2023-06-05 15:15:25 -0400

    Create concurrency.js

---
+++ src/clean-code/concurrency.js
@@ -0,0 +1,9 @@
+/**
+ * @desc
+ * =====
+ * Concurrency: JavaScript is a single-threaded language, which means it runs one task at a time. 
+ * - these threads are event-based and only triggered by the occurrence of events.
+ * - JavaScript's concurrency model is based on an event loop, which executes the code and processes events. 
+ * - JavaScript handles concurrency by default with an asynchronous programming model using event loops, callbacks, promises, or async/await. 
+ * =====
+ */
